5214ce66401d7b9e7e01ef28a81d16780c5d2df3
Max Villeneuve <maxvilleneuve@hotmail.com>
2019-09-08 17:32:09 -0400

    Made the core then up sequence

--- LED_Cube.py
+++ LED_Cube.py
@@ -1,5 +1,6 @@
 import RPi.GPIO as GPIO
 from time import sleep
+import random
 
 #Using BCM standard
 GPIO.setmode(GPIO.BCM)
@@ -15,60 +16,60 @@ GPIO.setup([5, 6, 13, 19], GPIO.OUT, initial=GPIO.LOW)
 #Declaring columns and rows
 column = [14, 15, 18, 23, 24, 25, 8, 7, 12, 16, 20, 21, 4, 17, 27, 22]
 row = [5, 6, 13, 19]
+#Shotcut
+out = GPIO.output
 
-#Outer shell, then core
+
+#1 corner dot into full cube
+#choose random dot on top level and then make it come down
+#Random dots anywhere
+
 #This resets all the pins
 def reset():
     for i in range(16):
-        GPIO.output(column[i], 1)
+        out(column[i], 1)
     for i in range(4):
-        GPIO.output(row[i], 0)
+        out(row[i], 0)
 
-#Lights the 4 walls
-def verticalWalls():
-    for i in range(4):
-        GPIO.output(row[i], 1)
-    for i in range(4):
-        GPIO.output(column[i], 0)
-    for i in range(12, 16, 1):
-        GPIO.output(column[i], 0)
-    for i in range(4, 9, 4):
-        GPIO.output(column[i], 0)
-    for i in range(7, 12, 4):
-        GPIO.output(column[i], 0)
-
-#Lights up the white LEDs in the middle
-def middleTopBottomLEDs():
-    for i in range(0, 4, 3):
-        GPIO.output(row[i], 1)
-    GPIO.output(column[5], 0)
-    GPIO.output(column[6], 0)
-    GPIO.output(column[9], 0)
-    GPIO.output(column[10], 0)
-
-#Lights up the blue LEDS
-def middleLED():
-    for i in range(1, 3, 1):
-        GPIO.output(row[i], 1)
-    GPIO.output(column[5], 0)
-    GPIO.output(column[6], 0)
-    GPIO.output(column[9], 0)
-    GPIO.output(column[10], 0)
-
-#The loop with the completed pattern
-for i in range(3):
-    for j in range(60):
-        reset()
-        verticalWalls()
-        sleep(0.01)
-        reset()
-        middleTopBottomLEDs()
-        sleep(0.01)
+#This drop in the core then moves up the sides, which slows down when coming up
+def sides():
     reset()
-    middleLED()
-    sleep(1)
+    for i in range(4):
+        out(column[i], 0)
+    for i in range(12, 16):
+        out(column[i], 0)
+    for i in range(4, 8, 3):
+        out(column[i], 0)
+    for i in range(8, 12, 3):
+        out(column[i], 0)
 
 
+for i in range(10):
+    reset()
+    for i in range(4):
+        out(row[i], 1)
+        out(column[5], 0)
+        out(column[6], 0)
+        out(column[9], 0)
+        out(column[10], 0)
+        sleep(0.1)
+        out(row[i], 0)
+
+    sides()
+    out(row[3], 1)
+    sleep(0.1)
+    out(row[2], 1)
+    out(row[3], 0)
+    sleep(0.2)
+    out(row[1], 1)
+    out(row[2], 0)
+    sleep(0.3)
+    out(row[0], 1)
+    out(row[1], 0)
+    sleep(0.2)
+    for i in range (16):
+        out(column[i], 0)
+    sleep(0.1)
 
 
 print("done")
